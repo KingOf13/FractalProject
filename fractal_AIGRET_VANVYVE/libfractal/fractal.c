62084072f906008a2676dc31ed076b988ae96d8e
Nicolas Vanvyve <nicolas.vanvyve@hotmail.com>
2016-05-01 18:53:05 +0200

    Modif makefile

--- fractal_AIGRET_VANVYVE/libfractal/fractal.c
+++ fractal_AIGRET_VANVYVE/libfractal/fractal.c
@@ -27,9 +27,6 @@ struct fractal *fractal_new(const char *name, int width, int height, double a, d
 
 void fractal_free(struct fractal *f)
 {
-  if(f==NULL){
-    return;
-  }
   free(f->grid);
   free(f->name);
   free(f);
