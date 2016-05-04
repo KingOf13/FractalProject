e56e56e76ae94d0b3f30ce4adc55fbaf6ab8ff40
Nicolas Vanvyve <nicolas.vanvyve@hotmail.com>
2016-05-04 14:15:23 +0200

    Begin test

--- fractal_AIGRET_VANVYVE/test/test1.c
+++ fractal_AIGRET_VANVYVE/test/test1.c
@@ -25,15 +25,19 @@ MAI 2016
 /*
 Idees de test
 
+Creer une nouvelle fractale
+vérifier la taille allouée??
+donner une grille deja faite et tester les valeur
+idem inversément
 verifier que le longueur est ok
 idem largeur
 idem a
 idem b
 
+Tester le format --> a mettre dans 
 
 
 */
-
 
 
 /*
@@ -48,29 +52,46 @@ void test_myfree_desalloc(void)
 */
 
 
+void test_getter(char *name, int w, int h, double a, double b)
+{
+  struct fractal *fract_test = fractal_new(name,w,h,a,b);
+  CU_ASSERT_EQUAL(fractal_get_width(fract_test),w);
+  CU_ASSERT_EQUAL(fractal_get_height(fract_test),h);
+  CU_ASSERT_EQUAL(fractal_get_a(fract_test),a);
+  CU_ASSERT_EQUAL(fractal_get_b(fract_test),b);
+}
+
+
 int setup(void)  { return 0; }
 int teardown(void) { return 0; }
 
 int main()
 {
-   CU_pSuite pSuite = NULL;
-   if (CUE_SUCCESS != CU_initialize_registry())
-      return CU_get_error();
+  char *name = (char *) malloc(strlen("fract_test") * sizeof(char));
+  name = "fract_test";
+  int w = 30;
+  int h = 30;
+  double a = 0.4;
+  double b = 0.6;
+  struct fractal *fract_test = fractal_new(name,w,h,a,b);
 
-   pSuite = CU_add_suite("Projet-Fractales", setup, teardown);
-   if (NULL == pSuite) {
-      CU_cleanup_registry();
-      return CU_get_error();
-   }
-/*
-   if ((NULL == CU_add_test(pSuite, "Myfree desalloc",test_myfree_desalloc)) ||
-       (NULL == CU_add_test(pSuite, "Mycalloc initialisation",test_mycalloc_init))
+  CU_pSuite pSuite = NULL;
+  if (CUE_SUCCESS != CU_initialize_registry())
+  return CU_get_error();
+
+  pSuite = CU_add_suite("Projet-Fractales", setup, teardown);
+  if (NULL == pSuite) {
+    CU_cleanup_registry();
+    return CU_get_error();
+  }
+
+   if ((NULL == CU_add_test(pSuite, "Test getter",test_getter)) ||
+       (NULL == CU_add_test(pSuite, "/* ... */ ", /* ... */ ))
      )
    {
      CU_cleanup_registry();
      return CU_get_error();
    }
-*/
 
    CU_basic_set_mode(CU_BRM_VERBOSE);
    CU_basic_run_tests();
